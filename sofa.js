422f0e7b6990196ddd918191003ca60a7d3917c4
Corey Schram <corey@coreymedia.com>
2011-05-21 15:32:04 -0500

    Added View/Show method functionality.

--- sofa.js
+++ sofa.js
@@ -1,4 +1,4 @@
-//
+﻿//
 // Author: Corey Schram <corey@coreymedia.com>
 // Description: Framework-agnostic library to conveniently access CouchDB through AJAX
 //
@@ -27,11 +27,29 @@
     xhr.setRequestHeader("Content-Type", "application/json");
     
     xhr.onreadystatechange = function () {
+      var json = false;
       if (xhr.readyState === 4) {
+        if (xhr.responseText.slice(0, 1) === "{" || xhr.responseText.slice(0, 1) === "[") {
+          json = true;
+        }
         if (xhr.status >= 200 && xhr.status < 300) {
-          options.success && options.success(JSON.parse(xhr.responseText), xhr.responseText);
+          if (options.success) {
+            if (json) {
+              options.success(JSON.parse(xhr.responseText));
+            } else {
+              options.success(xhr.responseText);
+            }
+          }
         } else {
-          options.error && options.error(xhr.status, xhr.responseText);
+          if (options.error) {
+            if (json) {
+              options.error(xhr.status, JSON.parse(xhr.responseText));
+            } else {
+              options.error(xhr.status, xhr.responseText);
+            }
+          } else {
+            throw "Uncaught AJAX Error (" + xhr.status + "): " + xhr.responseText;
+          }
         }
       }
     };
@@ -43,6 +61,34 @@
     }
     
     return xhr;
+  }
+  
+  // Generate a Query string
+  function queryString(params) {
+    var query = "?", param;
+    for (param in params) {
+      if (params.hasOwnProperty(param)) {
+        if (query.slice(-1) !== "?") {
+          query += "&";
+        }
+        query += param + "=";
+        switch (typeof params[param]) {
+        case "number":
+          query += params[param];
+          break;
+        case "string":
+          query += "\"" + params[param] + "\"";
+          break;
+        case "boolean":
+          query += params[param] ? "true" : "false";
+          break;
+        default:
+          query += JSON.stringify(params[param]);
+          break;
+        }
+      }
+    }
+    return query;
   }
   
   // Get the status of a CouchDB Server
@@ -63,13 +109,7 @@
       success : function (doc) {
         onSuccess(doc.uuids);
       },
-      error : function (status, response) {
-        if (onError) {
-          onError(status, response);
-        } else {
-          throw "Uncaught AJAX Error (" + status + "): " + response;
-        }
-      }
+      error : onError
     });
   };
   
@@ -130,28 +170,61 @@
   };
   
   // Save a document into the database
-  Sofa.DB.prototype.save = function (doc, options) {
+  Sofa.DB.prototype.save = function (doc, onSuccess, onError) {
     if (doc._id) {
       ajax({
         method : "PUT",
         url : this.db + doc._id,
         data : doc,
-        success : options.success,
-        error : options.error
+        success : onSuccess,
+        error : onError
       });
     } else {
-      var self = this;
-      Sofa.getUUIDs(1, function (uuids) {
-        doc._id = uuids[0];
-        ajax({
-          method : "PUT",
-          url : self.db + doc._id,
-          data : doc,
-          success : options.success,
-          error : options.error
-        });
+      ajax({
+        method : "POST",
+        url : this.db,
+        data : doc,
+        success : function (response) {
+          doc._id = response.id;
+          doc._rev = response.rev;
+          onSuccess(response);
+        },
+        error : onError
       });
     }
+  };
+  
+  // Run a View Method
+  Sofa.DB.prototype.view = function (options) {
+    var viewURL = this.db + "_design/" + options.doc + "/_view/" + options.view;
+    if (options.params) {
+      viewURL += queryString(options.params);
+    }
+    ajax({
+      method : "GET",
+      url : viewURL,
+      success : function (response) {
+        options.success(response.rows, response);
+      },
+      error : options.error
+    });
+  };
+  
+  // Run a Show Method
+  Sofa.DB.prototype.show = function (options) {
+    var showURL = this.db + "_design/" + options.doc + "/_show/" + options.show;
+    if (options.id) {
+      showURL += "/" + options.id;
+    }
+    if (options.params) {
+      showURL += queryString(options.params);
+    }
+    ajax({
+      method : "GET",
+      url : showURL,
+      success : options.success,
+      error : options.error
+    });
   };
   
   // Expose Sofa to the global object
