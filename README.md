44deaf25d6e58015a600b02ff14b3f582d84a090
Corey Schram <corey@coreymedia.com>
2011-06-18 09:44:06 -0500

    Added NodeJS Test.

--- README.md
+++
@@ -1,2 +0,0 @@
-SofaJS is a library that makes it easy to communicate with a CouchDB server from the front end.
-It is heavily in development.
--- sofa.js
+++ sofa.js
@@ -1,140 +1,97 @@
-//
-// Author: Corey Schram <corey@coreymedia.com>
-// Description: Framework-agnostic library to conveniently access CouchDB through AJAX
-//
-// Last Modified 5/18/2011
-//
-
-(function (window) {
+(function () {
   "use strict";
-  var Sofa = {};
+  var request,
+      requestDefaults = {
+        path : "/",
+        method : "GET",
+        contentType : "application/json"
+      };
   
-  // Convenience function to create an XHR object
-  function createXHR(callback) {
-    var xhr;
-
-    if (window.XMLHttpRequest) {
-      xhr = new XMLHttpRequest();
-    } else {
-      xhr = new ActiveXObject("Microsoft.XMLHTTP");
+  if (typeof module !== "undefined" && module.exports) {
+    //// NodeJS Setup ////
+    var http = require("http");
+    request = function (options) {
+      options.prototype = requestDefaults;
+      // Make HTTP Request
+      var req = http.request(options, function (res) {
+        res.setEncoding("utf8");
+        var body = "";
+        res.on("data", function (chunk) {
+          body += chunk;
+        });
+        res.on("end", function () {
+          if (body.slice(0, 1) === "{" || body.slice(0, 1) === "[") {
+            // If the response is JSON, parse it
+            options.callback(JSON.parse(body), res.statusCode);
+          } else {
+            // Otherwise return the response text as is
+            options.callback(body, res.statusCode);
+          }
+        });
+      });
+      
+      if (options.data) {
+        req.write(options.data);
+      }
+      req.end();
+    };
+  } else {
+    //// Browser Setup ////
+    if (typeof JSON === "undefined") {
+      console.error("JSON is undefined, json2.js or equivalent needs to be loaded.");
     }
 
-    if (callback) {
+    request = function (options) {
+      options.prototype = requestDefaults;
+      var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+      
+      var path = options.host;
+      if (options.port !== 80) {
+        path += ":" + options.port;
+      }
+      path += options.path;
+      
+      xhr.open(options.method, path, true);
+      xhr.setRequestHeader("Content-Type", options.contentType);
+      
       xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            callback(null, JSON.parse(xhr.responseText));
+          if (xhr.responseText.slice(0, 1) === "{" || xhr.responseText.slice(0, 1) === "[") {
+            // If the response is JSON, parse it
+            options.callback(xhr.status, JSON.parse(xhr.responseText));
           } else {
-            callback("Error Code " + xhr.status + ": " + xhr.responseText);
+            // Otherwise return the response text as is
+            options.callback(xhr.status, xhr.responseText);
           }
         }
       };
-    }
-
-    return xhr;
+      
+      if (options.data) {
+        xhr.send(JSON.stringify(options.data));
+      } else {
+        xhr.send();
+      }
+    };
   }
   
-  // Generate a random ID for a document
-  // Taken from jq-couchdb
-  function generateId() {
-    var rk = Math.floor(Math.random() * 4013);
-    return (10016486 + (rk * 22423));
-  }
-  
-  // Get the status of a CouchDB Server
-  // @param {String} server Address of the CouchDB Server
-  // @param {Function} callback Callback to call when the status is retrieved
-  // @type Function
-  Sofa.getStatus = function (server, callback) {
-    var xhr = createXHR(callback);
-    xhr.open("GET", server, true);
-    xhr.send();
-  };
-  
-  // Replicate a database
-  // @param {String} server Address of the CouchDB Server
-  // @param {String} source Name or Address of the source database
-  // @param {String} target Name or Address of the target database
-  // @param {Function} callback Callback to call when the replication is completed
-  // @type Function
-  Sofa.replicate = function (server, source, target, callback) {
-    var xhr = createXHR(callback);
-    xhr.open("POST", server, true);
-    xhr.send('{"source":"' + source + '","target":"' + target + '"}');
-  };
-  
-  // Database binding constructor
-  // @param {String} database Address of the CouchDB database
-  // @returns Database Object
-  // @type Function
-  Sofa.DB = function (database) {
-    if (!database) {
-      throw "Sofa Database constructor expects a database name argument.";
+  var Sofa = function (options) {
+    if (options.server.slice(-1) === "/") {
+      this.server = options.server.slice(0, this.server.length - 2);
+    } else {
+      this.server = options.server;
     }
     
-    this.db = database;
-    if (this.db.slice(-1) !== "/") {
-      this.db = this.db + "/";
-    }
+    this.port = options.port || 5984;
   };
   
-  // Create a Database
-  // @param {Function} callback Callback to call when the database is created
-  // @type Function
-  Sofa.DB.prototype.create = function (callback) {
-    var xhr = createXHR(callback);
-    xhr.open("PUT", this.db, true);
-    xhr.send();
+  Sofa.prototype.status = function (cb) {
+    request({
+      host : this.server,
+      port : this.port,
+      path : "/",
+      method : "GET",
+      callback : cb
+    });
   };
   
-  // Delete a Database
-  // @param {Function} callback Callback to call when the database is deleted
-  // @type Function
-  Sofa.DB.prototype.del = function (callback) {
-    var xhr = createXHR(callback);
-    xhr.open("DELETE", this.db, true);
-    xhr.send();
-  };
-  
-  // Get a document from the database
-  // @param {Mixed}(String/Integer) key Document key
-  // @param {Function} callback Callback to call when the document is retrieved
-  // @type Function
-  Sofa.DB.prototype.get = function (key, callback) {
-    var xhr = createXHR(callback);
-    xhr.open("GET", this.db + key, true);
-    xhr.send();
-  };
-  
-  // Save a document into the database
-  // @param {Object} doc Document to save
-  // @param {Function} callback Callback to call when the document is saved
-  // @type Function
-  Sofa.DB.prototype.save = function (doc, callback) {
-    var xhr = createXHR(callback), id;
-    if (doc._id) {
-      id = doc._id;
-    } else {
-      id = generateId();
-      doc._id = id;
-    }
-    xhr.open("PUT", this.db + id, true);
-    xhr.send(JSON.stringify(doc));
-  };
-  
-  // Make an arbitrary request to the database
-  // @param {String} method HTTP Method
-  // @param {String} path URL Path to send to
-  // @param {Object} body JSON Body to send
-  // @param {Function} callback Callback to call when the request in completed
-  // @type Function
-  Sofa.DB.prototype.request = function (method, path, body, callback) {
-    var xhr = createXHR(callback);
-    xhr.open(method, this.db + path, true);
-    xhr.send(body);
-  };
-  
-  // Expose Sofa to the global object
-  window.Sofa = Sofa;
-  
-}(this));
+}());
